117980df8cdc8e6e4838b4fe1436fe07bd75ff30
EJADA\melasser <melasser@ejada.com>
2023-12-12 16:05:55 +0300

    implement system parameter encryption

---
+++ PS-setup-api/src/main/java/com/Products/ps/setup/util/CryptoUtils.java
@@ -0,0 +1,71 @@
+package com.Products.ps.setup.util;
+
+import java.math.BigInteger;
+import java.security.Key;
+import java.security.MessageDigest;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class CryptoUtils {
+
+    private static final String ALGO = "AES";
+    public static String key="22C42C0914523569FE1445F9SS4E50R5";
+   
+    /**
+     * Encrypt a string with AES algorithm.
+     *
+     * @param data is a string
+     * @return the encrypted string
+     */
+    public static String encrypt(String data) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(data.getBytes());
+        return  Base64.getEncoder().encodeToString(encVal);
+    }
+
+    /**
+     * Decrypt a string with AES algorithm.
+     *
+     * @param encryptedData is a string
+     * @return the decrypted string
+     */
+    public static String decrypt(String encryptedData) throws Exception {
+        Key key = generateKey();
+        Cipher c = Cipher.getInstance(ALGO);
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = Base64.getDecoder().decode(encryptedData);
+        byte[] decValue = c.doFinal(decordedValue);
+        return new String(decValue);
+    }
+
+    /**
+     * Generate a new encryption key.
+     */
+    private static Key generateKey() throws Exception {
+        return new SecretKeySpec(key.getBytes(), ALGO);
+    }
+    
+    public static String sha1(String text) {
+    	try {
+	    	MessageDigest digest = MessageDigest.getInstance("SHA-1");
+	    	digest.reset();
+	    	digest.update(text.getBytes("utf8"));
+	    	return String.format("%040x", new BigInteger(1, digest.digest()));
+	    	
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+    	return null;
+    }
+    
+    public static void main(String[] args) throws Exception {
+    	String encrypted=encrypt("P@ssw0rd");
+    	String hashed=sha1("P@ssw0rd");
+    	System.out.println(encrypted);
+    }
+}
